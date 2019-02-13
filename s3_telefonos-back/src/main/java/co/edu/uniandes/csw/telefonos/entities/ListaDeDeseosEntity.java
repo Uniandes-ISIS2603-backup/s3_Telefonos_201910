a840ac0cd51bb653de41b6e9599095a9014783c0
Andres Felipe Daza Diaz <af.daza@uniandes.edu.co>
2019-02-13 18:54:21 -0500

    Implementado ListaDeDeseosEntity

---
+++ s3_telefonos-back/src/main/java/co/edu/uniandes/csw/telefonos/entities/ListaDeDeseosEntity.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.telefonos.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
+
+/**
+ *
+ * @author Andres Felipe Daza Diaz
+ */
+@Entity
+public class ListaDeDeseosEntity extends BaseEntity implements Serializable{
+    
+    @PodamExclude
+    @OneToMany(mappedBy = "listaDeDeseos")
+    private List<TabletEntity> tablets = new ArrayList<TabletEntity>();
+    
+    //@PodamExclude
+    //@OneToMany(mappedBy = "listaDeDeseos")
+    //private List<CelularEntity> celulares = new ArrayList<CelularEntity>();
+
+    public ListaDeDeseosEntity(){
+        
+    }
+    
+    
+    /**
+     * @return the tablets
+     */
+    public List<TabletEntity> getTablets() {
+        return tablets;
+    }
+
+    /**
+     * @param tablets the tablets to set
+     */
+    public void setTablets(List<TabletEntity> tablets) {
+        this.tablets = tablets;
+    }
+    
+}
