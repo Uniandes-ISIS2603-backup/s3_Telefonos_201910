37b465b9376150200b60bb6060fff24d86f7ff76
Rodrigo González <rj.gonzalez10@uniandes.edu.co>
2019-02-13 20:40:46 -0500

    se termino publicación dto

--- s3_telefonos-api/src/main/java/co/edu/uniandes/csw/telefonos/dtos/PublicacionDTO.java
+++ s3_telefonos-api/src/main/java/co/edu/uniandes/csw/telefonos/dtos/PublicacionDTO.java
@@ -5,10 +5,66 @@
  */
 package co.edu.uniandes.csw.telefonos.dtos;
 
+import co.edu.uniandes.csw.telefonos.entities.PublicacionEntity;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author rj.gonzalez10
  */
-public class PublicacionDTO {
+public class PublicacionDTO implements Serializable{
+    //fecha de creación de la publicación
+    private Date fechaCreacion;
+    //arreglo de imagenes que se van a  usar en el post
+    private ArrayList<String> imagenes; 
+    //id
+    private Long id;
     
+   public PublicacionDTO()
+   {
+       
+   }
+   public PublicacionDTO(PublicacionEntity publicacionEntity)
+   {
+       this.fechaCreacion = publicacionEntity.getFechaCreacion();
+       this.imagenes = publicacionEntity.getImagenes();
+       this.id = publicacionEntity.getId();
+       
+   }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public ArrayList<String> getImagenes() {
+        return imagenes;
+    }
+
+    public void setImagenes(ArrayList<String> imagenes) {
+        this.imagenes = imagenes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+   
+    public PublicacionEntity toEntity()
+    {
+        PublicacionEntity entidad = new PublicacionEntity();
+        entidad.setFechaCreacion(this.fechaCreacion);
+        entidad.setImagenes(this.imagenes);
+        entidad.setId(this.id);
+        return entidad;
+    }
+
 }
