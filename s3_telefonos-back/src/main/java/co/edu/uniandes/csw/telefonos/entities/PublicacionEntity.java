33b998cd7e26e6e6d4eb4cbed4942c4bd03d8f9c
Rodrigo González <rj.gonzalez10@uniandes.edu.co>
2019-03-02 16:17:17 -0500

    Se modela el atributo precio en publicación, se modelan las relaciones de publicación

--- s3_telefonos-back/src/main/java/co/edu/uniandes/csw/telefonos/entities/PublicacionEntity.java
+++ s3_telefonos-back/src/main/java/co/edu/uniandes/csw/telefonos/entities/PublicacionEntity.java
@@ -8,7 +8,10 @@ package co.edu.uniandes.csw.telefonos.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
 
 /**
  *
@@ -20,9 +23,15 @@ public class PublicacionEntity extends BaseEntity implements Serializable{
     private Date fechaCreacion;
     //arreglo de imagenes que se van a  usar en el post
     private ArrayList<String> imagenes;
+    //precio del producto ofrecido
+    private Double precio;
     //celular que esta asociado a la publicación, tiene que ser null en caso de que la publicación ya tenga asociada 
+    @OneToOne(mappedBy = "publicacion", cascade = CascadeType.ALL,
+              fetch = FetchType.EAGER, optional = false)
     private CelularEntity celular;
     //Tablet que esta aspciada a la publicación, tiene que ser null si tiene una publicación asociada
+    @OneToOne(mappedBy = "publicacion", cascade = CascadeType.ALL,
+              fetch = FetchType.EAGER, optional = false)
     private TabletEntity tablet;
 
     public Date getFechaCreacion() {
@@ -55,5 +64,12 @@ public class PublicacionEntity extends BaseEntity implements Serializable{
 
     public void setImagenes(ArrayList<String> imagenes) {
         this.imagenes = imagenes;
+    }
+     public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
     }
 }
