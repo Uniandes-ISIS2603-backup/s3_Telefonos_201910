12d403e02f6acc305fea7370c72a09f8c5f067af
Laura Prieto <lv.prieto@uniandes.edu.co>
2019-03-04 16:34:21 -0500

    SeguroResource Modificación

--- s3_telefonos-api/src/main/java/co/edu/uniandes/csw/telefonos/resources/SeguroResource.java
+++ s3_telefonos-api/src/main/java/co/edu/uniandes/csw/telefonos/resources/SeguroResource.java
@@ -6,6 +6,9 @@
 package co.edu.uniandes.csw.telefonos.resources;
 
 import co.edu.uniandes.csw.telefonos.dtos.SeguroDTO;
+import co.edu.uniandes.csw.telefonos.ejb.SeguroLogic;
+import co.edu.uniandes.csw.telefonos.entities.SeguroEntity;
+import co.edu.uniandes.csw.telefonos.exceptions.BusinessLogicException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -19,82 +22,114 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
  * @author Daniel Babativa, Andres Daza, Rodrigo Gonzalez y Laura Prieto
  */
-
 @Path("seguros")
 @Produces("application/json")
 @Consumes("application/json")
 @RequestScoped
 public class SeguroResource {
-    private static final Logger LOGGER= Logger.getLogger(SeguroResource.class.getName());
-    
-    //@Inject 
-    //private SeguroLogic seguroLogic;
-    
- /**
- * Obtiene todos los seguros
- * @return Lista de seguros
- */
-@GET
-public List<SeguroDTO> obtenerSeguros (){
-    List<SeguroDTO> result = new ArrayList();
-    SeguroDTO f = new SeguroDTO();
-    f.setAseguradora("A prueba");
-    result.add(f);
-    return result;
-}
 
+    private static final Logger LOGGER = Logger.getLogger(SeguroResource.class.getName());
 
-/**
- * Crea un nuevo seguro
- * @param seguro Nuevo seguro que se va a crear
- * @return seguro creado
- */
-@POST
-public SeguroDTO crearSeguro (SeguroDTO seguro){
-    //SeguroDTO seguroDTO = new SeguroDTO(seguroLogic)
-    return null;
-}
+    @Inject
+    private SeguroLogic seguroLogic;
 
-/**
- * Obtiene el seguro con identificador id
- * @param id Identificador del seguro
- * @return Seguro con identificador id
- */
-@GET
-@Path("{id: \\d+}")
- public SeguroDTO obtenerSeguroID(@PathParam("id") int id) {
-     SeguroDTO f =new SeguroDTO();
-     return f;
- }
-   
-/**
- * Actualiza un seguro con identificador id
- * @param id Identificador del seguro
- * @param seguro Nueva informacion del seguro
- * @return Seguro actualizado
- */ 
-@PUT
-@Path("{id: \\d+}")
- public SeguroDTO  actualizarSeguroID(@PathParam("id") int id, SeguroDTO seguro){
-     
-     return seguro;
- }
-  
- /**
-  * Elimina un seguro con identificador id
-  * @param id Identificador del seguro
-  * @return Seguro eliminado
-  */
- @DELETE
- @Path("{id: \\d+}")
- public SeguroDTO  eliminarSeguroID(@PathParam("id") int id){
-     SeguroDTO f =new SeguroDTO();
-     return f;
- }
+    /**
+     * Obtiene todos los seguros
+     *
+     * @return Lista de seguros
+     */
+    @GET
+    public List<SeguroDTO> obtenerSeguros() {
+        List<SeguroDTO> listaSeguros = listEntity2DTO(seguroLogic.getSeguros());
+        return listaSeguros;
+    }
+
+    /**
+     * Crea un nuevo seguro
+     *
+     * @param seguro Nuevo seguro que se va a crear
+     * @return seguro creado
+     */
+    @POST
+    public SeguroDTO crearSeguro(SeguroDTO seguro) throws BusinessLogicException {
+        SeguroDTO nuevoSeguroDTO = new SeguroDTO(seguroLogic.createSeguro(seguro.toEntity()));
+        return nuevoSeguroDTO;
+    }
+
+    /**
+     * Obtiene el seguro con identificador id
+     *
+     * @param id Identificador del seguro
+     * @return Seguro con identificador id
+     */
+    @GET
+    @Path("{id: \\d+}")
+    public SeguroDTO obtenerSeguroID(@PathParam("id") Long id) {
+        SeguroEntity seguroEntity = seguroLogic.getSeguro(id);
+        if (seguroEntity == null) {
+            throw new WebApplicationException("El recurso /seguros/" + id + " no existe.", 404);
+        }
+        SeguroDTO seguroDTO = new SeguroDTO(seguroEntity);
+        return seguroDTO;
+    }
+
+    /**
+     * Actualiza un seguro con identificador id
+     *
+     * @param id Identificador del seguro
+     * @param seguro Nueva informacion del seguro
+     * @return Seguro actualizado
+     */
+    @PUT
+    @Path("{id: \\d+}")
+    public SeguroDTO actualizarSeguroID(@PathParam("id")Long id, SeguroDTO seguro) throws BusinessLogicException {
+
+        seguro.setId(id);
+        if (seguroLogic.getSeguro(id) == null) {
+            throw new WebApplicationException("El recurso /seguros/" + id + " no existe.", 404);
+        }
+        SeguroDTO DTO = new SeguroDTO(seguroLogic.updateSeguro(id, seguro.toEntity()));
+        return DTO;
+    }
+
+    /**
+     * Elimina un seguro con identificador id
+     *
+     * @param id Identificador del seguro
+     * @return Seguro eliminado
+     */
+    @DELETE
+    @Path("{id: \\d+}")
+    public void eliminarSeguroID(@PathParam("id") Long id) {
+        SeguroEntity entity = seguroLogic.getSeguro(id);
+        if (entity == null) {
+            throw new WebApplicationException("El recurso /seguros/" + id + " no existe.", 404);
+        }
+        seguroLogic.deleteSeguro(id);
+    }
     
+    /**
+     * Convierte una lista de entidades a DTO.
+     *
+     * Este método convierte una lista de objetos SeguroEntity a una lista de
+     * objetos SegurolDTO (json)
+     *
+     * @param entityList corresponde a la lista de seguros de tipo Entity que
+     * vamos a convertir a DTO.
+     * @return la lista de seguros en forma DTO (json)
+     */
+    private List<SeguroDTO> listEntity2DTO(List<SeguroEntity> entityList) {
+        List<SeguroDTO> list = new ArrayList<>();
+        for (SeguroEntity entity : entityList) {
+            list.add(new SeguroDTO(entity));
+        }
+        return list;
+    }
+
 }
