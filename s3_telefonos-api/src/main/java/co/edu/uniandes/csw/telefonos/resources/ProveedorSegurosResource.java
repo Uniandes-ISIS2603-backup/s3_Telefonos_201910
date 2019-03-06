30f7fcfa52f64e8572352ca9871ac2df5fba07e3
Laura Prieto <lv.prieto@uniandes.edu.co>
2019-03-06 15:11:27 -0500

    ProveedorSeguro Resource terminado

---
+++ s3_telefonos-api/src/main/java/co/edu/uniandes/csw/telefonos/resources/ProveedorSegurosResource.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.telefonos.resources;
+
+import co.edu.uniandes.csw.telefonos.dtos.SeguroDTO;
+import co.edu.uniandes.csw.telefonos.ejb.ProveedorLogic;
+import co.edu.uniandes.csw.telefonos.ejb.ProveedorSegurosLogic;
+import co.edu.uniandes.csw.telefonos.ejb.SeguroLogic;
+import co.edu.uniandes.csw.telefonos.entities.SeguroEntity;
+import co.edu.uniandes.csw.telefonos.exceptions.BusinessLogicException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.WebApplicationException;
+
+/**
+ *
+ * @author Laura Valentina Prieto Jimenez
+ */
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class ProveedorSegurosResource {
+
+    private static final Logger LOGGER = Logger.getLogger(ProveedorSegurosResource.class.getName());
+
+    @Inject
+    private ProveedorLogic proveedorLogic;
+    
+    @Inject
+    private SeguroLogic seguroLogic;
+    
+    @Inject 
+    private ProveedorSegurosLogic proveedorSegurosLogic;
+    
+    /**
+     * Relaciona un seguro a un proveedor
+     *
+     * @param proveedorId Identificador del proveedor con el que se quiere
+     * relacionar el seguro
+     * @param seguroId Identificador del seguro que se quiere asociar a un
+     * proveedor
+     * @return Seguro relacionado con el proveedor
+     */
+    @POST
+    @Path("{seguroId: \\d+}")
+    public SeguroDTO agregarSeguro(@PathParam("proveedorId") Long proveedorId, @PathParam("seguroId") Long seguroId) {
+        if (proveedorLogic.getProveedor(proveedorId) == null) {
+            throw new WebApplicationException("El recurso /proveedores/" + proveedorId + " no existe.", 404);
+        }
+        SeguroDTO seguroDTO = new SeguroDTO(proveedorSegurosLogic.addSeguro(proveedorId, seguroId));
+        return seguroDTO;
+    }
+
+    /**
+     * Obtiene todos los seguros asociados a un proveedor
+     * @param proveedorId Identificador del proveedor
+     * @return 
+     */
+    @GET
+    public List<SeguroDTO> obtenerSeguros(@PathParam("proveedorId") Long proveedorId) {
+        List<SeguroDTO> listaDetailDTOs = segurosListEntity2DTO(proveedorSegurosLogic.getSeguros(proveedorId));
+        return listaDetailDTOs;
+    }
+    
+    /**
+     * Obtiene un seguro con identificador dado de un proveedor con identificador dado
+     * @param proveedorId Identificador del proveedor
+     * @param seguroId Identificador del seguro al que se quiere acceder
+     * @return 
+     */
+    @GET
+    @Path("{seguroId: \\d+}")
+    public SeguroDTO obtenerSeguro(@PathParam("proveedorId") Long proveedorId, @PathParam("seguroId") Long seguroId) throws BusinessLogicException {
+        if (seguroLogic.getSeguro(seguroId) == null) {
+            throw new WebApplicationException("El recurso /proveedores/" + proveedorId + "/seguros/" + seguroId + " no existe.", 404);
+        }
+        SeguroDTO seguroDTO = new SeguroDTO(proveedorSegurosLogic.getSeguro(proveedorId, seguroId));
+        return seguroDTO;
+    }
+    
+    /**
+     * Convierte una lista de SeguroEntity a una lista de SeguroDTO.
+     *
+     * @param entityList Lista de SeguroEntity a convertir.
+     * @return Lista de SeguroDTO convertida.
+     */
+    private List<SeguroDTO> segurosListEntity2DTO(List<SeguroEntity> entityList) {
+        List<SeguroDTO> list = new ArrayList();
+        for (SeguroEntity entity : entityList) {
+            list.add(new SeguroDTO(entity));
+        }
+        return list;
+    }
+}
---
+++ s3_telefonos-back/src/main/java/co/edu/uniandes/csw/telefonos/ejb/ProveedorSegurosLogic.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.telefonos.ejb;
+
+import co.edu.uniandes.csw.telefonos.entities.ProveedorEntity;
+import co.edu.uniandes.csw.telefonos.entities.SeguroEntity;
+import co.edu.uniandes.csw.telefonos.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.telefonos.persistence.ProveedorPersistence;
+import co.edu.uniandes.csw.telefonos.persistence.SeguroPersistence;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Laura Valentina Prieto Jimenez
+ */
+@Stateless
+public class ProveedorSegurosLogic {
+    
+    @Inject
+    private ProveedorPersistence proveedorPersistence;
+    
+    @Inject
+    private SeguroPersistence seguroPersistence;
+    
+    
+    /**
+     * Agregar un seguro al proveedor
+     *
+     * @param proveedorId El id del proveedor en el cual se va a guardar el seguro
+     * @param seguroId El id del seguro que se va a guardar
+     * @return Seguro creado.
+     */
+    public SeguroEntity addSeguro(Long proveedorId, Long seguroId) {
+        ProveedorEntity proveedorEntity = proveedorPersistence.find(proveedorId);
+        SeguroEntity seguroEntity = seguroPersistence.find(seguroId);
+        List<SeguroEntity> seguros = proveedorEntity.getSeguros();
+        seguros.add(seguroEntity);
+        proveedorEntity.setSeguros(seguros);
+        return seguroEntity;
+    }
+    
+    /**
+     * Retorna todos los seguros asociados a un proveedor
+     *
+     * @param proveedorId El ID del proveedor buscado
+     * @return La lista de seguros del proveedor
+     */
+    public List<SeguroEntity> getSeguros(Long proveedorId) {
+        return proveedorPersistence.find(proveedorId).getSeguros();
+    }
+    
+    /**
+     * Retorna un seguro asociado a un proveedor
+     *
+     * @param proveedorId El id del proveedor a buscar.
+     * @param seguroId El id del seguro a buscar
+     * @return El seguro encontrada dentro del proveedor.
+     * @throws BusinessLogicException Si el seguro no esta asociado al proveedor
+     */
+    public SeguroEntity getSeguro(Long proveedorId, Long seguroId) throws BusinessLogicException {
+        List<SeguroEntity> seguros = proveedorPersistence.find(proveedorId).getSeguros();
+        SeguroEntity seguroEntity = seguroPersistence.find(seguroId);
+        int index = seguros.indexOf(seguroEntity);
+        if (index >= 0) {
+            return seguros.get(index);
+        }
+        throw new BusinessLogicException("El seguro no esta asociado al proveedor");
+    }
+
+    
+}
