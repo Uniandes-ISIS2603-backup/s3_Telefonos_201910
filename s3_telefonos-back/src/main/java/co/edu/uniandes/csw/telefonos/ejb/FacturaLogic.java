d341a982fb0b42e52a5b12cc317ea6bab3c91a5b
Laura Prieto <lv.prieto@uniandes.edu.co>
2019-03-06 16:01:28 -0500

    Revision Logic de comprador, proveedor, seguro y factura

--- s3_telefonos-back/src/main/java/co/edu/uniandes/csw/telefonos/ejb/FacturaLogic.java
+++ s3_telefonos-back/src/main/java/co/edu/uniandes/csw/telefonos/ejb/FacturaLogic.java
@@ -18,26 +18,37 @@ import javax.inject.Inject;
  */
 @Stateless
 public class FacturaLogic {
+
     @Inject
     private FacturaPersistence persistencia;
-    
+
     /**
      * Crea una nueva factura con la informacion por parametro
+     *
      * @param factura Informacion de la factura que se va a crear
      * @return Factura creada
      * @throws BusinessLogicException Si la factura a persistir ya existe.
      */
-    public FacturaEntity createFactura (FacturaEntity factura) throws BusinessLogicException{
-        
+    public FacturaEntity createFactura(FacturaEntity factura) throws BusinessLogicException {
+
         //La referencia de cada factura debe ser unica
-        if(persistencia.findByReferencia(factura.getReferencia())!=null){
+        if (persistencia.findByReferencia(factura.getReferencia()) != null) {
             throw new BusinessLogicException("Ya existe una Factura con la referencia \"" + factura.getReferencia() + "\"");
         }
-        
+        if (factura.getComprador() == null) {
+            throw new BusinessLogicException("No se puede crear una factura sin comprador");
+        }
+        if (factura.getProveedor() == null) {
+            throw new BusinessLogicException("No se puede crear una factura sin proveedor");
+        }
+        if (factura.getPublicacion() == null) {
+            throw new BusinessLogicException("No se puede crear una factura sin publicacion");
+        }
+
         factura = persistencia.create(factura);
         return factura;
     }
-    
+
     /**
      *
      * Obtener todas los facturas existentes en la base de datos.
@@ -45,10 +56,10 @@ public class FacturaLogic {
      * @return una lista de facturas.
      */
     public List<FacturaEntity> getFacturas() {
-       List<FacturaEntity> facturas = persistencia.findAll();
+        List<FacturaEntity> facturas = persistencia.findAll();
         return facturas;
     }
-    
+
     /**
      *
      * Obtener una factura por medio de su id.
@@ -57,31 +68,38 @@ public class FacturaLogic {
      * @return la factura solicitada por medio de su id.
      */
     public FacturaEntity getFactura(Long facturaId) {
-       FacturaEntity facturaEntity = persistencia.find(facturaId);
+        FacturaEntity facturaEntity = persistencia.find(facturaId);
         return facturaEntity;
     }
-    
+
     /**
      *
      * Actualizar una factura.
      *
-     * @param facturaId: id de la factura para buscarla en la base de
-     * datos.
-     * @param facturaEntity: factura con los cambios para ser actualizada,
-     * por ejemplo la referencia.
+     * @param facturaId: id de la factura para buscarla en la base de datos.
+     * @param facturaEntity: factura con los cambios para ser actualizada, por
+     * ejemplo la referencia.
      * @return la factura con los cambios actualizados en la base de datos.
      */
     public FacturaEntity updateFactura(Long facturaId, FacturaEntity facturaEntity) throws BusinessLogicException {
-        
+
         //La referencia de cada factura debe ser unica
-        if(persistencia.findByReferencia(facturaEntity.getReferencia())!=null){
+        if (persistencia.findByReferencia(facturaEntity.getReferencia()) != null) {
             throw new BusinessLogicException("Ya existe una Factura con la referencia \"" + facturaEntity.getReferencia() + "\"");
         }
-        
-       FacturaEntity newEntity = persistencia.update(facturaEntity);
+        if (facturaEntity.getComprador() == null) {
+            throw new BusinessLogicException("No se puede actualizar una factura sin comprador");
+        }
+        if (facturaEntity.getProveedor() == null) {
+            throw new BusinessLogicException("No se puede actualizar una factura sin proveedor");
+        }
+        if (facturaEntity.getPublicacion() == null) {
+            throw new BusinessLogicException("No se puede actualizar una factura sin publicacion");
+        }
+        FacturaEntity newEntity = persistencia.update(facturaEntity);
         return newEntity;
     }
-    
+
     /**
      * Borrar una factura
      *
--- s3_telefonos-back/src/main/java/co/edu/uniandes/csw/telefonos/ejb/SeguroLogic.java
+++ s3_telefonos-back/src/main/java/co/edu/uniandes/csw/telefonos/ejb/SeguroLogic.java
@@ -20,43 +20,47 @@ import javax.inject.Inject;
  */
 @Stateless
 public class SeguroLogic {
-    
+
     @Inject
-        private SeguroPersistence persistence;
-    
+    private SeguroPersistence persistence;
+
     private static final Logger LOGGER = Logger.getLogger(SeguroLogic.class.getName());
-    
-    public SeguroEntity createSeguro(SeguroEntity seguro)throws BusinessLogicException{
+
+    public SeguroEntity createSeguro(SeguroEntity seguro) throws BusinessLogicException {
+        if (seguro.getProveedor() == null) {
+            throw new BusinessLogicException("No se puede crear un seguro sin proveedor");
+        }
         seguro = persistence.create(seguro);
         return seguro;
     }
-    
-    public List<SeguroEntity> getSeguros(){
-         LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los seguros");
+
+    public List<SeguroEntity> getSeguros() {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los seguros");
         List<SeguroEntity> seguros = persistence.findAll();
         LOGGER.log(Level.INFO, "Termina proceso de consultar todos los seguros");
         return seguros;
     }
-    
-    public SeguroEntity getSeguro(Long seguroId){
+
+    public SeguroEntity getSeguro(Long seguroId) {
         LOGGER.log(Level.INFO, "Inicia proceso de consultar el seguro con id = {0}", seguroId);
         SeguroEntity seguro = persistence.find(seguroId);
-        if(seguro == null){
+        if (seguro == null) {
             LOGGER.log(Level.SEVERE, "El seguro con identificador = {0} no existe", seguroId);
         }
         LOGGER.log(Level.INFO, "Termina proceso de consultar el seguro con identificador = {0}", seguroId);
         return seguro;
     }
-    
+
     public SeguroEntity updateSeguro(Long seguroId, SeguroEntity seguroEntity) throws BusinessLogicException {
-        
-       SeguroEntity newEntity = persistence.update(seguroEntity);
+        if (seguroEntity.getProveedor() == null) {
+            throw new BusinessLogicException("No se puede actualizar un seguro sin proveedor");
+        }
+        SeguroEntity newEntity = persistence.update(seguroEntity);
         return newEntity;
     }
-    
+
     public void deleteSeguro(Long seguroId) {
         persistence.delete(seguroId);
     }
-    
-    
+
 }
